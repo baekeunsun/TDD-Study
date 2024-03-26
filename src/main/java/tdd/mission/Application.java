8a34585b1f6a0dd71434be77cdcd81533389ab64
백은선 <72916415+baekeunsun@users.noreply.github.com>
2024-03-26 21:00:45 +0900

    Fix : 자판기 상품정보 Scanner로 받도록 수정

--- src/main/java/tdd/mission/Application.java
+++ src/main/java/tdd/mission/Application.java
@@ -17,6 +17,30 @@ public class Application {
         for (Map.Entry<Coin, Integer> entry : coinMap.entrySet()) {
             System.out.println(entry.getKey().getAmount() + "원 - " + entry.getValue() + "개");
         }
+
+        System.out.println("상품명과 가격, 수량을 입력해 주세요. (예: [콜라,1500,20];[사이다,1000,10])");
+        scanner.nextLine(); // 버퍼 비우기
+        String productInput = scanner.nextLine();
+
+        registerProductsFromInput(vendingMachine, productInput);
+
+        System.out.println("자판기가 보유한 상품");
+        for (Product product : vendingMachine.getProducts()) {
+            System.out.println("상품명: " + product.getName() + ", 가격: " + product.getPrice() + "원, 수량: " + product.getQuantity());
+        }
+    }
+
+    private static void registerProductsFromInput(VendingMachine vendingMachine, String input) {
+        String[] productsInfo = input.split(";");
+        for (String productInfo : productsInfo) {
+            productInfo = productInfo.substring(1, productInfo.length() - 1);
+            String[] info = productInfo.split(",");
+            String name = info[0];
+            int price = Integer.parseInt(info[1]);
+            int quantity = Integer.parseInt(info[2]);
+            Product product = new Product(name, price, quantity);
+            vendingMachine.addProduct(product);
+        }
     }
 
 }
